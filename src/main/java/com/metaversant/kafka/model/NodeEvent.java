63e728d7308cd5c9806b7809f1614d252a86a01c
Jeff Potts <jeffpotts01@gmail.com>
2018-09-20 10:56:53 -0500

    Add no args constructor to NodeEvent so it will serialize/deserialize properly in a Kafka listener. Upgrade SDK to 3.0.1. Change Alfresco dependencies to 5.2.g/f.

--- src/main/java/com/metaversant/kafka/model/NodeEvent.java
+++ src/main/java/com/metaversant/kafka/model/NodeEvent.java
@@ -1,7 +1,9 @@
 package com.metaversant.kafka.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -10,8 +12,10 @@ import java.util.Date;
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NodeEvent {
-    public static enum EventType {
+    public enum EventType {
         CREATE,
         UPDATE,
         DELETE,
